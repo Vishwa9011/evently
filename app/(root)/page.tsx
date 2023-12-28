c97e66c345c0f2fbccbf6ce017135a760cb7e8bd
Vishwa Vivek Yadav <112635764+Vishwa9011@users.noreply.github.com>
2023-12-28 18:45:30 +0530

    by following added backend and added webhooks

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,33 @@
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main>
-      <h1 className='text-2xl'>Evently</h1>
-      <Button variant={"default"} className='m-5'>Hello</Button>
-    </main>
+    <>
+      <section className='bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10'>
+        <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
+          <div className='flex flex-col justify-center gap-8'>
+            <h1 className="h1-bold">Host, Connect, Celebrate: Your Events, Our Platform!</h1>
+            <p className='p-regular-20 md:p-regular-24'>
+              Book and learn helpful tips from 3,168+ mentors in world-class companies with our global community.
+            </p>
+            <Button size={"lg"} asChild className='w-full button sm:w-fit'>
+              <Link href={"#events"}>Explore now</Link>
+            </Button>
+          </div>
+          <Image src={"/assets/images/hero.png"} alt='hero' width={1000} height={1000} className='max-h-[70vh] object-contain 2xl:max-h-[50vh]' />
+        </div>
+      </section>
+
+      <section id='#events' className='wrapper my-8 flex flex-col gap-89 md:gap-12'>
+        <h2 className='h2-bold'>Trusted by <br /> Thousands of Events</h2>
+        <div className="flex flex-col gap-5 w-full md:flex-row">
+          Search
+          CategoryFilter
+        </div>
+      </section>
+
+    </>
   )
 }
--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,7 +1,9 @@
-import { SignedOut } from "@clerk/nextjs";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import { Button } from "@components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
+import NavItems from "./NavItems";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
@@ -11,7 +13,19 @@ const Header = () => {
           <Image src={"/assets/images/logo.svg"} width={128} height={38} alt="Evently Logo" />
         </Link>
 
+        <SignedIn>
+          <nav className="md:flex-between hidden w-full max-w-xs">
+            <NavItems />
+          </nav>
+        </SignedIn>
+
+
         <div className="flex w-32 justify-end gap-3">
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+            <MobileNav />
+          </SignedIn>
+
           <SignedOut>
             <Button asChild className="rounded-full">
               <Link href={"/sign-in"}>
