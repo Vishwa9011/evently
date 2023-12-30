3648749dee8262d7cb1a754d96458c57d8d2e301
Vishwa Vivek Yadav <112635764+Vishwa9011@users.noreply.github.com>
2023-12-30 12:58:29 +0530

    logo changed

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer className="bordet-t ">
       <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
         <Link href={'/'}>
-          <Image src={'/assets/images/logo.svg'} alt="logo" width={128} height={38} />
+          <Image src={'/assets/images/logo.png'} alt="logo" width={128} height={38} />
         </Link>
         <p>2023 Evently. All Rights reserved.</p>
       </div>
--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -8,13 +8,13 @@ import MobileNav from "./MobileNav";
 const Header = () => {
   return (
     <header className="w-full border-b">
-      <div className="wrapper flex items-start justify-between">
+      <div className="wrapper flex items-center justify-between">
         <Link href={"/"} className="w-36">
-          <Image src={"/assets/images/logo.svg"} width={128} height={38} alt="Evently Logo" />
+          <Image src={"/assets/images/logo.png"} width={128} height={38} alt="Evently Logo" />
         </Link>
 
         <SignedIn>
-          <nav className="md:flex-between hidden w-full max-w-xs">
+          <nav className="md:flex-between hidden w-full max-w-xs items-center">
             <NavItems />
           </nav>
         </SignedIn>
